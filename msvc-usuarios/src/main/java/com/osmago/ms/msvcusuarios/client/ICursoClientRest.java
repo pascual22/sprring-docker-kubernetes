140af5d25ead5dff63add7d7c641bfb7eb1533ea
Oscar Gonzalez <osmaogonzalez@gmail.com>
2022-11-02 20:56:15 -0500

    Kubernetes

--- msvc-usuarios/src/main/java/com/osmago/ms/msvcusuarios/client/ICursoClientRest.java
+++ msvc-usuarios/src/main/java/com/osmago/ms/msvcusuarios/client/ICursoClientRest.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-cursos", url = "${msvc.cursos.url}")
+@FeignClient(name = "msvc-cursos")
 public interface ICursoClientRest {
 
     @DeleteMapping("/eliminar-curso-usuario/{id}")
